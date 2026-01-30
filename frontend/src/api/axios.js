e234c1e2511572aeb44a4e9ba55b7ad2cfccfc4a
Prabhav <prabhav589@gmail.com>
2026-01-30 18:49:15 +0530

    updated backend url to local host

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -3,7 +3,7 @@ import CryptoJS from 'crypto-js';
 
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL  || "http://localhost:3000/api";
-
+console.log("API Base URL:", API_BASE_URL);
 
 const api = axios.create({
   baseURL: API_BASE_URL,
